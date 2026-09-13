8cc32541c3e09655420c544f971ca69934018f53
koulierakis <koulierakis@jsaglobal.gr>
2026-09-13 23:57:01 +0300

    feat: add app configuration

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const APP_VERSION = '2.3.0-free-exercise-db';
+export const APP_NAME = 'Fitness Trainer';
+export const DEFAULT_LANGUAGE = 'el-GR';
+export const DEFAULT_VOICE_MODE = 'Coach';
