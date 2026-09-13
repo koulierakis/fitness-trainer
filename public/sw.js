42ac8a85a3ad175cb8675ff9f359e062a2039663
koulierakis <koulierakis@jsaglobal.gr>
2026-09-13 23:56:53 +0300

    feat: add PWA service worker

---
+++ public/sw.js
@@ -0,0 +1,1 @@
+const CACHE='fitness-trainer-v2.3';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/fitness-trainer/','/fitness-trainer/manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
