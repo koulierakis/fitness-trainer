a3f103f15cc26344b5656ed7522c3b0823cbcd6f
koulierakis <koulierakis@jsaglobal.gr>
2026-09-14 00:33:45 +0300

    chore: bump Fitness Trainer to v2.4

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = '2.3.0-free-exercise-db';
+export const APP_VERSION = '2.4.0-repdb-illustrations';
 export const APP_NAME = 'Fitness Trainer';
 export const DEFAULT_LANGUAGE = 'el-GR';
 export const DEFAULT_VOICE_MODE = 'Coach';
