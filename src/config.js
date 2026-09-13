59d95be6dda7cde8982ab49a41a8f10915689345
koulierakis <koulierakis@jsaglobal.gr>
2026-09-14 00:48:48 +0300

    chore: bump Fitness Trainer to v2.5 anatomical preview build

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = '2.4.0-repdb-illustrations';
+export const APP_VERSION = '2.5.0-anatomical-preview';
 export const APP_NAME = 'Fitness Trainer';
 export const DEFAULT_LANGUAGE = 'el-GR';
 export const DEFAULT_VOICE_MODE = 'Coach';
