762bc3868af0a0e4cbd6749870e5c06db482eba0
koulierakis <koulierakis@jsaglobal.gr>
2026-09-14 10:54:15 +0300

    chore: mark AscendAPI ExerciseDB live test build

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = '2.5.0-anatomical-preview';
+export const APP_VERSION = '2.6.0-ascendapi-test';
 export const APP_NAME = 'Fitness Trainer';
 export const DEFAULT_LANGUAGE = 'el-GR';
 export const DEFAULT_VOICE_MODE = 'Coach';
